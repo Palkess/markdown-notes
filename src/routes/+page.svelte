68765443605ee9ddf36ca95bf1d5e1e517ab63dd
Jonas Andersson <jonas.andersson.web@gmail.com>
2024-02-02 19:51:50 +0100

    Initial commit, using Tauri example setup with sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api";
+
+    let greetMsg = '';
+
+    async function greeting(name: string) {
+        greetMsg = await invoke('greet', { name });
+    }
+</script>
+
+<button on:click={() => greeting('jonas')}>Hälsa</button>
+<button on:click={() => greetMsg = ''}>Reset</button>
+
+{greetMsg}
